388ecd935a9b2bed15f429b6bb0093fd848a5fdd
amc1988 <9002amc@gmail.com>
2023-01-15 16:41:58 +0200

    System.Data.Linq added

--- LinqConsoleApp1/Program.cs
+++ LinqConsoleApp1/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
 
-namespace LinqConsoleApp1
+
+namespace LinqConsoleApp2
 {
     class Program
     {
+        static string connectionString = "Server=ASUS-PC41;Database=userdb;Trusted_Connection=True;";
         static void Main(string[] args)
         {
+            using (DataContext db = new DataContext(connectionString))
+            {
+                //// Получаем таблицу пользователей
+                Table<User> users = db.GetTable<User>();
+
+                var query = from u in users
+                            where u.Age > 25
+                            orderby u.FirstName
+                            select u;
+
+                foreach (var user in query)
+                {
+                    Console.WriteLine("{0} \t{1} \t{2}", user.Id, user.FirstName, user.Age);
+                }
+
+                Console.Read();
+            }
+
         }
     }
 }
---
+++ LinqConsoleApp1/User.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Linq.Mapping;
+
+namespace LinqConsoleApp2
+{
+    [Table(Name = "Users")]
+    public class User
+    {
+        [Column(IsPrimaryKey = true, IsDbGenerated = true, CanBeNull = false)]
+        public int Id { get; set; }
+
+        [Column(Name = "Name")]
+        public string FirstName { get; set; }
+
+        [Column]
+        public int Age { get; set; }
+    }
+
+}
